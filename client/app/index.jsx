3dbd93eaa9f923d5f6eb9f48744f60dadaf643c8
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-05 15:24:07 -0800

    Set up React environment / add first component

---
+++ client/app/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './components/AwesomeComponent.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+      <p> Hello React!! COOOOL</p>
+
+      <AwesomeComponent />
+      </div>
+
+
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
