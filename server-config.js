170c300ded27e6363600d261ce0f17e2e2d08365
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-06 22:58:23 -0800

    heroku / bug

--- server-config.js
+++ server-config.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 app.get('/', function(req, res) {
-  res.render('./client/app/index.jsx');
+  res.render('./client/app/index.html');
 });
 
 
