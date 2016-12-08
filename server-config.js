ab51eccc050d9b7c6cf17fe6ea12547cd2fe7f75
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-07 18:15:38 -0800

    fix path

--- server-config.js
+++ server-config.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.use('/', express.static(path.join(__dirname, 'client')));
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'client', 'index.html')); });
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '..', 'client', 'index.html')); });
 
 app.post('/addFriend', (req, res) => {
   var username = ({friend: req.body.data});
