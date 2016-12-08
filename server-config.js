aa3158916572ea1821183397d774c42603cf7b93
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-07 18:41:55 -0800

    fix path

--- server-config.js
+++ server-config.js
@@ -15,7 +15,7 @@ var Friend = require('./App/Models/Friend.js');
 // Path to help express serve public files
 var path = require('path');
 
-// app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // app.set('views', __dirname + './client');
 // app.engine('html', engines.mustache);
@@ -27,13 +27,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 
 
-// app.get('/', function(req, res) {
-//   res.sendFile('./client/index.html');
-// });
+app.get('/', function(req, res) {
+  res.sendFile('./client/index.html');
+});
 
-app.use(express.static(path.join(__dirname, 'client')));
+// app.use('/', express.static(path.join(__dirname, 'client')));
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '..', 'client', 'index.html')); });
+// app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '..', 'client', 'index.html')); });
 
 app.post('/addFriend', (req, res) => {
   var username = ({friend: req.body.data});
